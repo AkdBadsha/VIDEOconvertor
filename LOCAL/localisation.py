71834f5da5667ae6ca77b9e85020972c031f8944
Bᴀᴅsʜᴀ Sᴛᴜᴅɪᴏs <141768309+AkdBadsha@users.noreply.github.com>
2023-08-09 07:50:33 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -1,15 +1,15 @@
 #Don't be a thief by stealing other's Hardwork, it took time and effort to make this repo.
 #Respect my work by not making any changes here.
 
-START_TEXT = "Send me any file to begin."
+START_TEXT = "Tʜɪs Bᴏᴛ Is ᴏɴʟʏ Fᴏʀ Pᴇʀsᴏɴᴀʟ Usᴇ , Sᴇɴᴅ ᴍᴇ ᴀɴʏ ғɪʟᴇ ᴛᴏ ʙᴇɢɪɴ.Pᴏᴡᴇʀᴇᴅ Bʏ : Aᴋᴅ Dᴜʙʙᴇʀs"
 
 FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
 
-CHANNEL_LINK = "https://t.me/DroneBots"
+CHANNEL_LINK = "https://t.me/AKD_DUBBERS"
 
-SUPPORT_LINK = "https://t.me/TeamDrone"
+SUPPORT_LINK = "https://t.me/+vU1eN4sTQlkyOGM9"
 
-info_text = "This bot is developed by @MaheshChauhan\n\nWritten in python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.4"   
+info_text = "This bot is developed by @kingBadsha3232\n\nWritten in python library TELETHON.\n\nBot by : @Badsha_Studios\nSupport : @kingBadsha3232\n\nV1.4"   
 
 help_text = """**v1.4**
 
@@ -27,15 +27,15 @@ help_text = """**v1.4**
 
 •`Trim` - cut your videos"""
 
-source_text = "**Deploy your own bot**"
+source_text = "**Hᴇʀᴇ's Sᴏᴍᴇ Iɴғᴏʀᴍᴀᴛɪᴏɴ Aʙᴏᴜᴛ Oᴜʀ Cᴏᴍᴍᴜɴɪᴛʏ**"
 
-DEV = "https://t.me/MaheshChauhan"
+DEV = "https://t.me/kingBadsha3232"
 
 spam_notice = "This bot is hosted on heroku, and hence can just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
 
 JPG = "LOCAL/video_convertor.jpg"
 
-JPG0 = 'https://telegra.ph/file/d98c559b56ef884ef3bad.jpg'
+JPG0 = 'https://telegra.ph/file/7d9fa3a72d7f00dab8a38.jpg'
 
 JPG2 = "LOCAL/20211215_165751.jpg"
 
